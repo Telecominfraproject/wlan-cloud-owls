70b32ad199e7bc07ea5725051521e84d3a810031
stephb9959 <stephane.bourque@gmail.com>
2021-04-07 23:03:44 -0700

    Initial commit

--- src/StatsDisplay.cpp
+++ src/StatsDisplay.cpp
@@ -16,7 +16,7 @@ void StatsDisplay::run() {
     {
         Poco::Thread::sleep(3000);
         Stats()->Report(R);
-        std::cout << "Connected:" << std::setw(6) << R.Connected << " TX:" << std::setw(10) << R.TX << " RX:" << std::setw(10) << R.RX <<
-        " IN:" << std::setw(9) << R.InMsgs << " OUT:" << std::setw(9) << R.OutMsgs << std::endl;
+        std::cout << "Connected:" << std::setw(6) << R.Connected << " TX:" << std::setw(13) << R.TX << " RX:" << std::setw(13) << R.RX <<
+        " IN:" << std::setw(13) << R.InMsgs << " OUT:" << std::setw(13) << R.OutMsgs << std::endl;
     }
 }

480eceeb77c3130e73a3c108a2978a10d994cca0
Johann Hoffmann <johann.hoffmann@mailbox.org>
2021-11-16 20:50:18 +0100

    Add Dockerfile and related config
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

---
+++ docker-entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+if [ "$SELFSIGNED_CERTS" = 'true' ]; then
+    update-ca-certificates
+fi
+
+if [[ "$TEMPLATE_CONFIG" = 'true' && ! -f "$OWLS_CONFIG"/owls.properties ]]; then
+  ROOTCA=${ROOTCA:-"\$OWLS_ROOT/certs/root.pem"} \
+  ISSUER=${ISSUER:-"\$OWLS_ROOT/certs/issuer.pem"} \
+  CERT=${CERT:-"\$OWLS_ROOT/certs/device-cert.pem"} \
+  KEY=${KEY:-"\$OWLS_ROOT/certs/device-key.pem"} \
+  CLIENTCAS=${CLIENTCAS:-"\$OWLS_ROOT/certs/clientcas.pem"} \
+  CAS=${CAS:-"\$OWLS_ROOT/certs/cas"} \
+  KEY_PASSWORD=${KEY_PASSWORD:-"mypassword"} \
+  RESTAPI_HOST_ROOTCA=${RESTAPI_HOST_ROOTCA:-"\$OWLS_ROOT/certs/restapi-ca.pem"} \
+  RESTAPI_HOST_PORT=${RESTAPI_HOST_PORT:-"16004"} \
+  RESTAPI_HOST_CERT=${RESTAPI_HOST_CERT:-"\$OWLS_ROOT/certs/restapi-cert.pem"} \
+  RESTAPI_HOST_KEY=${RESTAPI_HOST_KEY:-"\$OWLS_ROOT/certs/restapi-key.pem"} \
+  RESTAPI_HOST_KEY_PASSWORD=${RESTAPI_HOST_KEY_PASSWORD:-"mypassword"} \
+  INTERNAL_RESTAPI_HOST_ROOTCA=${INTERNAL_RESTAPI_HOST_ROOTCA:-"\$OWLS_ROOT/certs/restapi-ca.pem"} \
+  INTERNAL_RESTAPI_HOST_PORT=${INTERNAL_RESTAPI_HOST_PORT:-"17004"} \
+  INTERNAL_RESTAPI_HOST_CERT=${INTERNAL_RESTAPI_HOST_CERT:-"\$OWLS_ROOT/certs/restapi-cert.pem"} \
+  INTERNAL_RESTAPI_HOST_KEY=${INTERNAL_RESTAPI_HOST_KEY:-"\$OWLS_ROOT/certs/restapi-key.pem"} \
+  INTERNAL_RESTAPI_HOST_KEY_PASSWORD=${INTERNAL_RESTAPI_HOST_KEY_PASSWORD:-"mypassword"} \
+  SERVICE_KEY=${SERVICE_KEY:-"\$OWLS_ROOT/certs/restapi-key.pem"} \
+  SERVICE_KEY_PASSWORD=${SERVICE_KEY_PASSWORD:-"mypassword"} \
+  SYSTEM_DATA=${SYSTEM_DATA:-"\$OWLS_ROOT/data"} \
+  SYSTEM_URI_PRIVATE=${SYSTEM_URI_PRIVATE:-"https://localhost:17007"} \
+  SYSTEM_URI_PUBLIC=${SYSTEM_URI_PUBLIC:-"https://localhost:16007"} \
+  SYSTEM_URI_UI=${SYSTEM_URI_UI:-"http://localhost"} \
+  KAFKA_ENABLE=${KAFKA_ENABLE:-"true"} \
+  KAFKA_BROKERLIST=${KAFKA_BROKERLIST:-"localhost:9092"} \
+  STORAGE_TYPE=${STORAGE_TYPE:-"sqlite"} \
+  STORAGE_TYPE_POSTGRESQL_HOST=${STORAGE_TYPE_POSTGRESQL_HOST:-"localhost"} \
+  STORAGE_TYPE_POSTGRESQL_USERNAME=${STORAGE_TYPE_POSTGRESQL_USERNAME:-"owls"} \
+  STORAGE_TYPE_POSTGRESQL_PASSWORD=${STORAGE_TYPE_POSTGRESQL_PASSWORD:-"owls"} \
+  STORAGE_TYPE_POSTGRESQL_DATABASE=${STORAGE_TYPE_POSTGRESQL_DATABASE:-"owls"} \
+  STORAGE_TYPE_POSTGRESQL_PORT=${STORAGE_TYPE_POSTGRESQL_PORT:-"5432"} \
+  STORAGE_TYPE_MYSQL_HOST=${STORAGE_TYPE_MYSQL_HOST:-"localhost"} \
+  STORAGE_TYPE_MYSQL_USERNAME=${STORAGE_TYPE_MYSQL_USERNAME:-"owls"} \
+  STORAGE_TYPE_MYSQL_PASSWORD=${STORAGE_TYPE_MYSQL_PASSWORD:-"owls"} \
+  STORAGE_TYPE_MYSQL_DATABASE=${STORAGE_TYPE_MYSQL_DATABASE:-"owls"} \
+  STORAGE_TYPE_MYSQL_PORT=${STORAGE_TYPE_MYSQL_PORT:-"3306"} \
+  envsubst < /owls.properties.tmpl > $OWLS_CONFIG/owls.properties
+fi
+
+if [ "$1" = '/openwifi/owls' -a "$(id -u)" = '0' ]; then
+    if [ "$RUN_CHOWN" = 'true' ]; then
+      chown -R "$OWLS_USER": "$OWLS_ROOT" "$OWLS_CONFIG"
+    fi
+    exec su-exec "$OWLS_USER" "$@"
+fi
+
+exec "$@"
